102ca49e8db147134f4fc392ac1afd9048de4a9d
Yuxuan <nine.slide@gmail.com>
2022-10-13 17:09:45 +0200

    check if the input field is touched

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,14 +1,15 @@
 import { useState, useRef } from 'react';
 
 const SimpleInput = (props) => {
-  /**
-   * 'which one should we use? 
-   * 'useRef is we only need the value once
-   * 'useState if we need to reset every input, or for every instance
-   */
+    /**
+     * 'which one should we use?
+     * 'useRef is we only need the value once
+     * 'useState if we need to reset every input, or for every instance
+     */
     // const nameInputRef = useRef();//' if we are using useRef
     const [enteredName, setEnteredName] = useState('');
-    const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
+    const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+    const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
     const nameInputHandler = (e) => {
         setEnteredName(e.target.value);
@@ -16,20 +17,25 @@ const SimpleInput = (props) => {
 
     const formSubmissionHandler = (e) => {
         e.preventDefault();
-
+        setEnteredNameTouched(true);
         if (enteredName.trim() === '') {
-          setEnteredNameIsValid(false);
-          return;
+            setEnteredNameIsValid(false);
+            return;
         }
-
-        setEnteredNameIsValid(true)
+        setEnteredNameIsValid(true);
         console.log(enteredName);
         // const enteredValue = nameInputRef.current.value;//' if we are using useRef
         // console.log(enteredValue);//' if we are using useRef
-        setEnteredName('')//.if we are clearing the input field
+        setEnteredName(''); //.if we are clearing the input field
     };
 
-    const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched; //# only when user tried to inout and the input is invalid, then we can say the name input is invalid
+
+    const nameInputClasses = nameInputIsInvalid
+        ? 'form-control invalid'
+        : 'form-control';
+
+
     return (
         <form onSubmit={formSubmissionHandler}>
             <div className={nameInputClasses}>
@@ -39,10 +45,12 @@ const SimpleInput = (props) => {
                     type="text"
                     id="name"
                     onChange={nameInputHandler}
-                    value={enteredName} //.if we are clearing the input field
+                    value={enteredName} //.if we are clearing the input field 'twoway binding'
                 />
             </div>
-            {!enteredNameIsValid && <p className='error-text'>'must enter a valid name'</p> }
+            {nameInputIsInvalid && (
+                <p className="error-text">'must enter a valid name'</p>
+            )}
             <div className="form-actions">
                 <button>Submit</button>
             </div>
