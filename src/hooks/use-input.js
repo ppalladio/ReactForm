64cef566787f7cba51eff2c75d47928af8ec7f83
Yuxuan <nine.slide@gmail.com>
2022-10-17 14:49:29 +0200

    switch hook to useReducer

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,30 +1,74 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
+// const useInput = (validateValue) => {
+//     const [enteredValue, setEnteredValue] = useState('');
+//     const [isTouched, setIsTouched] = useState(false);
+
+//     const valueIsValid = validateValue(enteredValue);
+//     const hasError = !valueIsValid && isTouched;
+
+//     const valueChangeHandler = (e) => {
+//         setEnteredValue(e.target.value);
+//     };
+
+//     const lostFocusHandler = (_e) => {
+//         setIsTouched(true);
+//     };
+//     const resetFunc =()=>{
+//         setEnteredValue('')
+//         setIsTouched(false)
+//     }
+//     return {
+//         value: enteredValue,
+//         isValid:valueIsValid,
+//         hasError: hasError,
+//         valueChangeHandler,
+//         lostFocusHandler,
+//         resetFunc
+//     };
+// };
+
+//::   usereducer                :
+import { useReducer } from 'react';
+const init = {
+    value: '',
+    isTouched: false,
+};
+const inputReducer = (state, action) => {
+    if (action.type === 'INPUT') {
+        return { value: action.value, isTouched: state.isTouched };
+    }
+    if (action.type === 'BLUR') {
+        return { isTouched: true, value: state.value };
+    }
+    if (action.type === 'RESET') {
+        return { isTouched: false, value: '' };
+    }
+    return init;
+};
 const useInput = (validateValue) => {
-    const [enteredValue, setEnteredValue] = useState('');
-    const [isTouched, setIsTouched] = useState(false);
+    const [inputState, dispatchInput] = useReducer(inputReducer, init);
 
-    const valueIsValid = validateValue(enteredValue);
-    const hasError = !valueIsValid && isTouched;
+    const valueIsValid = validateValue(inputState.value);
+    const hasError = !valueIsValid && inputState.isTouched;
 
     const valueChangeHandler = (e) => {
-        setEnteredValue(e.target.value);
+        dispatchInput({ type: 'INPUT', value: e.target.value });
     };
 
-    const lostFocusHandler = (_e) => {
-        setIsTouched(true);
+    const lostFocusHandler = (e) => {
+        dispatchInput({ type: 'BLUR' });
     };
-    const resetFunc =()=>{
-        setEnteredValue('')
-        setIsTouched(false)
-    }
+    const resetFunc = () => {
+        dispatchInput({ type: 'RESET' });
+    };
     return {
-        value: enteredValue,
-        isValid:valueIsValid,
+        value: inputState.value,
+        isValid: valueIsValid,
         hasError: hasError,
         valueChangeHandler,
         lostFocusHandler,
-        resetFunc
+        resetFunc,
     };
 };
 
